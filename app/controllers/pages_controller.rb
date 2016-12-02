77a79310fdb89e2513fcde6ca4c7285c4939166a
Tyler Polzin <tyler@strategicretailsolutions.com>
2016-12-02 02:45:22 +0000

    Call Sheet work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
     def home
     end
     
+    def colonial_elegance_callsheet
+    end
+    
     def about
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'colonial_elegance_callsheet', to: 'pages#colonial_elegance_callsheet'
   
 end
 
